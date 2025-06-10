e350505972cf84fc7aa8449ed776ee6194699e39
MOUHAMADI MOGNE <mmogne@salle1>
2025-06-10 13:46:42 +0200

    test

--- controllers/logincontrollers.php
+++ controllers/logincontrollers.php
@@ -1,12 +1,26 @@
 <?php
-session_start();
-$username = $_POST['username'] ?? '';
-$password = $_POST['password'] ?? '';
 
-// À remplacer par une vraie vérification dans un modèle (models/)
-if ($username === 'admin' && $password === 'admin') {
-    $_SESSION['user'] = $username;
-    header('Location: ../views/dashboard.php');
-} else {
-    echo "Identifiants incorrects.";
+function connexion() {
+    require('models/connection.php');
+    $c = connection();
+    require('models/family_crud.php');
+    $families = find_all_families($c);
+    
+    //View display
+    require('views/families_view.php');
+    families_view($families);
+}
+
+
+function family_print_ctrl() {
+    require('models/connection.php');
+    $c = connection();
+    require('models/equipment_crud.php');
+    $id = $_GET['id'];
+    $equipments = find_equipments_by_family_id($c, $id);
+    
+    //View display
+    require('views/family_view.php');
+    family_view($equipments);
+    
 }
--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-
 <?php
-header('Location: views/login.php');
-exit;
+    //The front root controller
 
+    //SEULEMENT en phase de développement !
+    error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+    ini_set('display_errors', 1);
+    
+    //The requested route
+    $route = null;
+    if (isset($_GET['route'])) {
+        $route = $_GET['route'];
+    }
+    
+    //We switch to the good controller
+    switch ($route) {
+        case null:
+
+            require('views/login.php');
+            break;
+            
+        
+        default:
+            require('views/404_view.php');
+            break;
+            
+    }
