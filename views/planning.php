10bcfae63329e7d8f2d8dad4009de7babe170e0c
etudiant <etudiant@debian>
2025-06-14 14:51:33 +0200

    modif

--- views/planning.php
+++ views/planning.php
@@ -5,7 +5,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require 'views/header.php';
-?> 
+
+?>
+
 <div class="container">
     <h2>Planning - 24 dernières heures</h2>
 
@@ -15,13 +17,12 @@ require 'views/header.php';
             <th>Prénom</th>
             <th>Mission</th>
             <th>Lieu</th>
-            <th>Affectation</th>
+            <th>Statut</th>
             <th>Date</th>
         </tr>
         <?php foreach ($plannings as $p): ?>
             <?php
                 $classe = 'affectation-' . strtolower(str_replace([' ', "'", '"'], '-', $p['affectation']));
-                echo "<!-- Classe générée : $classe -->"; // Affiche la classe générée dans le code source
             ?>
             <tr class="<?= $classe ?>">
                 <td><?= htmlspecialchars($p['sauveteur_nom']) ?></td>
@@ -33,6 +34,18 @@ require 'views/header.php';
             </tr>
         <?php endforeach; ?>
     </table>
+
+
+<?php 
+if ($_SESSION['user']['profil'] === 'gestionnaire'): 
+?>
+
+<div class="action-buttons">
+    <a href="index.php?page=modifier_planning" class="btn mouton">Modifier le planning</a>
+    <a href="index.php?page=ajouter_sauveteur" class="btn mouton">Ajouter un sauveteur</a>
+    <a href="index.php?page=supprimer_sauveteur" class="btn mouton">Supprimer un sauveteur</a>
+</div>
+<?php endif; ?>
 
     <div class="legend">
         <h3>Légende des affectations</h3>
@@ -48,13 +61,6 @@ require 'views/header.php';
     </div>
 </div>
 
-<?php if ($_SESSION['user']['profil'] === 'gestionnaire'): ?>
-    <div class="action-buttons">
-        <a href="index.php?page=modifier_planning" class="btn mouton">Modifier le planning</a>
-        <a href="index.php?page=ajouter_sauveteur" class="btn mouton">Ajouter un sauveteur</a>
-        <a href="index.php?page=supprimer_sauveteur" class="btn mouton">Supprimer un sauveteur</a>
-    </div>
-<?php endif; ?>
 
 
 
