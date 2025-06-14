bc5f231cb1acf45569dbe67093ae748bad5cfe27
etudiant <etudiant@debian>
2025-06-14 16:25:45 +0200

    f

--- views/admin.php
+++ views/admin.php
@@ -15,3 +15,14 @@
 
 
 <?php require 'views/footer.php'; ?>
+
+
+<div class="action-buttons">
+    <a href="index.php?page=ajouter_utilisateur" class="btn mouton">Ajouter un utilisateur</a>
+    <a href="index.php?page=supprimer_utilisateur" class="btn mouton">Supprimer un utilisateur</a>
+</div>
+<?php if ($_SESSION['user']['profil'] === 'administration'): ?>
+    <div class="action-buttons">
+        <a href="index.php?page=modifier_utilisateur" class="btn mouton">Modifier un utilisateur</a>
+    </div>
+<?php endif; ?>
